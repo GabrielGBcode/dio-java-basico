c896b34a78790c90fdf9184f3c35309a8336faf1
Gabriel GB <105217214+GabrielGBcode@users.noreply.github.com>
2023-10-09 18:08:23 -0300

    Created identacao files

--- java-anatomia-de-classes/src/Indentacao.java
+++ java-anatomia-de-classes/src/Indentacao.java
@@ -1,6 +1,6 @@
 public class Indentacao {
     public static void main(String[] args) {
-        int mediaFinal = 6;
+        int mediaFinal = 7;
         if (mediaFinal < 6) {
             System.out.println("REPROVADO!");
         } else if (mediaFinal == 6) {

9f2ebf4d21729abc923071524998e9f98da670ab
Gabriel GB <105217214+GabrielGBcode@users.noreply.github.com>
2023-10-10 09:15:08 -0300

    Types & Variables

---
+++ java-anatomia-de-classes/src/TiposDeVariaveis.java
@@ -0,0 +1,13 @@
+public class TiposDeVariaveis {
+    public static void main(String[] args) {
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        // final determina uma constante
+
+        final double PI = 3.14;
+
+        // Impossível usar: double PI = 2.1;
+    }
+}
